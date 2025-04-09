4b75b38e7eea3371be78e359ef7c2ef86167a2be
Matheus Amon <amonmatheus757@gmail.com>
2025-04-09 19:21:47 -0300

    feat: add initial cart context and initial cart component

---
+++ src/app/[slug]/menu/components/cart-sheet.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useContext } from "react";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+
+import { CartContext } from "../contexts/cart";
+
+const CartSheet = () => {
+  const { isOpen, toggleCart } = useContext(CartContext);
+  
+  return (
+    <Sheet open={isOpen} onOpenChange={toggleCart}>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Teste</SheetTitle>
+          <SheetDescription>Teste</SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default CartSheet;
