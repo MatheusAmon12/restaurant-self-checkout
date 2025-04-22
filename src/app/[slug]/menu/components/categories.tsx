d2de8cd81082fb312a5b41d13bd8ac602fa227ca
Matheus Amon <amonmatheus757@gmail.com>
2025-04-22 15:56:23 -0300

    fix: ensure that products summary is shown only if exist products on cart

--- src/app/[slug]/menu/components/categories.tsx
+++ src/app/[slug]/menu/components/categories.tsx
@@ -87,7 +87,7 @@ const RestaurantCategories: React.FC<RestaurantCategoriesProps> = ({
 
       <h3 className="px-5 pt-2 font-semibold">{selectedCategory.name}</h3>
       <Products products={selectedCategory.products} />
-      {products.length && (
+      {products.length > 0 && (
         <div className="fixed bottom-0 left-0 right-0 flex w-full items-center justify-between border-t bg-white px-5 py-3">
           <div>
             <p className="text-xs text-muted-foreground">Total dos pedidos</p>
